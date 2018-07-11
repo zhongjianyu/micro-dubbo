ad0d9223b8454056a63694672ca613655de13bf5
zhongjianyu <949434438@qq.com>
2018-07-11 19:59:40 +0800

    add

--- dubbo-consumer01/src/main/java/dubbo/consumer01/Main.java
+++ dubbo-consumer01/src/main/java/dubbo/consumer01/Main.java
@@ -1,15 +1,19 @@
 package dubbo.consumer01;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * 应用启动类
  * 
  * @author zhongjy
  *
  */
+@SpringBootApplication(scanBasePackages = "dubbo.consumer01")
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SpringApplication.run(Main.class, args);
 
 	}
 
--- dubbo-provider01/src/main/java/dubbo/provider01/Main.java
+++ dubbo-provider01/src/main/java/dubbo/provider01/Main.java
@@ -1,15 +1,22 @@
 package dubbo.provider01;
 
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 /**
  * 应用启动类
  * 
  * @author zhongjy
  *
  */
+@SpringBootApplication
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new SpringApplicationBuilder(Main.class)
+        .web(WebApplicationType.NONE)
+        .run(args);
 
 	}
 
